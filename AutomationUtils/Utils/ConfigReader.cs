ebf81f0b6d5d929b750ead6c064a18c8f7da8f3c
Vitalii Dolotov <vitaliy.dolotov@gmail.com>
2021-02-17 18:30:13 +0200

    Added ConfigReader

---
+++ AutomationUtils/Utils/ConfigReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AutomationUtils.Utils
+{
+    public static class ConfigReader
+    {
+        private static string ConfigFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "appsettings.json");
+
+        public static string ByKey(string key)
+        {
+            using (StreamReader sr = new StreamReader(ConfigFilePath))
+            {
+                try
+                {
+                    string configFileContent = sr.ReadToEnd();
+
+                    var responseContent = JsonConvert.DeserializeObject<JObject>(configFileContent);
+                    string value = responseContent[key].ToString();
+
+                    return value;
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"Unable to read configuration property for '{key}' key: {e}");
+                }
+            }
+        }
+    }
+}
