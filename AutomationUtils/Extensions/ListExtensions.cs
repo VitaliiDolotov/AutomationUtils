113f1c8ad86c4e69318391ffb461b1e9ff77d14e
Vitalii Dolotov <vitaliy.dolotov@gmail.com>
2021-09-14 19:12:04 +0300

    Added ToString List extension

---
+++ AutomationUtils/Extensions/ListExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AutomationUtils.Extensions
+{
+    public static class ListExtensions
+    {
+        public static string ToString(this List<string> list, string separator)
+        {
+            return list.Any() ? string.Join(separator, list.ToArray()) : string.Empty;
+        }
+    }
+}
