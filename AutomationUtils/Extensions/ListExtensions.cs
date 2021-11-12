c45efda5454a9d2598a80bda38ba9ffb4c1b9402
Vitalii Dolotov <vitaliy.dolotov@gmail.com>
2021-11-12 20:26:50 +0200

    Added GetRandomItem

--- AutomationUtils/Extensions/ListExtensions.cs
+++ AutomationUtils/Extensions/ListExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AutomationUtils.Extensions
@@ -8,6 +9,13 @@ namespace AutomationUtils.Extensions
         public static string ToString(this List<string> list, string separator)
         {
             return list.Any() ? string.Join(separator, list.ToArray()) : string.Empty;
+        }
+
+        public static T GetRandomItem<T>(this IEnumerable<T> list)
+        {
+            var listInstance = list.ToArray();
+            var index = new Random().Next(listInstance.Count());
+            return listInstance.ToArray()[index];
         }
     }
 }
