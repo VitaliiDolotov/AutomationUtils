3d0470407a4629d612ad3aaf87678c294a576c01
Vitalii Dolotov <vitaliy.dolotov@gmail.com>
2021-12-30 14:21:32 +0200

    Updated RestClientExtensions to use IBaseApiMethods

--- AutomationUtils/Extensions/RestClientExtensions.cs
+++ AutomationUtils/Extensions/RestClientExtensions.cs
@@ -5,7 +5,7 @@ namespace AutomationUtils.Extensions
 {
     public static class RestClientExtensions
     {
-        public static T InitApiMethods<T>(this RestClient client) where T : BaseApiMethods, new()
+        public static T InitApiMethods<T>(this RestClient client) where T : IBaseApiMethods, new()
         {
             var requestMethods = new T { Client = client };
             return requestMethods;
